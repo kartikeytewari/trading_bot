5d5cc6f4a5d814450a760879ae7fd59e8e62c19c
Kartikey Tewari <kartikeya30@gmail.com>
2021-04-10 00:38:26 +0530

    add makefile and price_generator.cpp code

---
+++ Makefile
@@ -0,0 +1,9 @@
+default:
+	make build
+	make run
+
+build:
+	g++-10 -std=c++17 -Wshadow -Wall -Wno-unused-result -fsanitize=address -fsanitize=undefined -fmax-errors=1 -g -D_GLIBCXX_DEBUG -o price_generator price_generator.cpp 
+
+run:
+	./price_generator 10000 1 > price_val.txt
---
+++ price_generator.cpp
@@ -0,0 +1,32 @@
+/*
+**  Author: Kartikey Tewari
+**  Time: 10-04-2021  00:34:43
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int rand (int a, int b)
+{
+    return (rand()%(b-a+1))+a;
+}
+
+// first argument: number of data values
+// second argument: seed value
+int main(int count, char* inp[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    srand(stoi(inp[2]));
+    int n=stoi(inp[1]);
+
+    cout << n << endl;
+    for (int i=0;i<=n-1;i++)
+    {
+        cout << (rand()%10000)+1 << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
